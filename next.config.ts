920d85015f12713950d9bb910d6c3ac492cc32c8
Affan Raza <affanraza@Affans-MacBook-Air.local>
2025-09-16 19:13:55 +0530

    updated nextjs

--- next.config.ts
+++ next.config.ts
@@ -1,17 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
-  theme: {
-  extend: {
-    colors: {
-      brandBrown: "#421C05",
-      brandOrange: "#E5973D",
-      brandText: "#2C1810",
-    },
-  },
-},
-
+  /* next.js config options here */
 };
 
 export default nextConfig;
